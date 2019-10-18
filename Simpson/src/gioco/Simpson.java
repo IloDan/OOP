08020ebc1b3036f2901eaf20b8c44f3d1e3413c6
JasGu25 <jasmine.guglielmi@gmail.com>
2019-10-18 20:03:35 +0200

    ciaoooo

--- Simpson/src/gioco/Simpson.java
+++ Simpson/src/gioco/Simpson.java
@@ -1,7 +1,10 @@
 package gioco;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.*;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 
@@ -13,16 +16,27 @@ public class Simpson extends Canvas implements Runnable{
 	
 	public boolean running;
 	
-	public void start() {
+	private void tick() {}
+	private void render() {
+		BufferStrategy bs=getBufferStrategy();
+		if(bs==null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g=bs.getDrawGraphics();
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.dispose();
+		bs.show();
+	}
+	private void start() {
 		if(running)
 			return;
 		running=true;
 		new Thread(this, "SimpsonMain-Thread").start();
 	}
-	public void stop() {
-		double target=60.0;
-		double nsPerTick=1000000000.0/target;
-		long lastTime;
+	private void stop() {
 		if(!running)
 			return;
 		running=false;
@@ -30,8 +44,42 @@ public class Simpson extends Canvas implements Runnable{
 	}
 	@Override
 	public void run() {
+		double target=60.0;
+		double nsPerTick=1000000000.0/target;
+		long lastTime=System.nanoTime();
+		long timer=System.currentTimeMillis();
+		double unprocessed=0.0;
+		int fps=0;
+		int tps=0;
+		boolean canRender=false;
+		
 		while(running) {
+			long now= System.nanoTime();
+			unprocessed+=(now-lastTime)/nsPerTick;
+			lastTime=now;
 			
+			if(unprocessed>=1.0) {
+				tick();
+				unprocessed--;
+				tps++;
+				canRender=true;
+			}else canRender=false;
+			
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			if(canRender) {
+				render();
+				fps++;
+			}
+			if(System.currentTimeMillis()-1000>timer) {
+				timer+=1000;
+				System.out.printf("FPS: %d| TPS: %d\n",fps,tps);
+				fps=0;
+				tps=0;
+			}
 		}
 		System.exit(0);
 	}
@@ -55,5 +103,4 @@ public class Simpson extends Canvas implements Runnable{
 		frame.requestFocus();
 		game.start();
 	}
-	//simpsonre
 }
